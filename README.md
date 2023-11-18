f61256d6a8bd0e0c75a0759d4a8e350a9a04ca6c
Max Lapan <max.lapan@gmail.com>
2023-11-18 20:20:32 +0100

    Base outline

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 Various utilities for RuTracker
+
+
+# cue_gen
+
+Generates spoilers data from image+.cue rip dirs.
+
+Modes of operation (given with `-m` parameter):
+* `full`: every cue produces separate spoiler with tracks, performers, logs and cues sections
+* `titles`: (default) every cue produces just tracks and performers sections
+* `logs`: every cue produces logs and cues spoilers
--- rt_tools/scripts/cue_gen.py
+++ rt_tools/scripts/cue_gen.py
@@ -2,11 +2,65 @@
 Utility to generate rutracker classical music release from CUE files
 """
 import argparse
+import pathlib
+import typing as tt
+from rt_tools.cueparser import CueSheet
+
+HEADER_OUTPUT = "%performer% - %title%\n%file%\n%tracks%"
+TRACK_OUTPUT = "%performer% - %title%"
+
+PathCue = tt.Tuple[pathlib.Path, CueSheet]
+
+
+def load_cue(cue_path: pathlib.Path) -> PathCue:
+    cue = CueSheet()
+    cue.setOutputFormat(HEADER_OUTPUT, TRACK_OUTPUT)
+    cue.setData(cue_path.read_text('utf-8'))
+    cue.parse()
+    return cue_path, cue
+
+
+def generate_titles(cue: CueSheet, composers_mode: str) -> tt.Generator[str, None, None]:
+    yield ''
+
+
+def get_section_name(idx: int, path: pathlib.Path) -> str:
+    # if path contains dir name, use dir name, otherwise just index
+    if len(path.parts) > 1:
+        return path.parts[-2]
+    return f"CD{idx}"
+
+
+def generate_output(mode: str, paths_cues: tt.List[PathCue],
+                    composers_mode: str) -> tt.Generator[str, None, None]:
+    for idx, (path, cue) in enumerate(paths_cues, start=1):
+        section_name = get_section_name(idx, path)
+        yield f'[spoiler="{section_name} - [:]"]'
+        if mode == 'titles':
+            yield from generate_titles(cue, composers_mode=composers_mode)
+        yield '[/spoiler]'
 
 
 def main() -> int:
     parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--mode", choices=('full', 'titles', 'logs'), default='titles',
+                        help="Mode of generation, default=titles")
+    parser.add_argument("-c", "--composers", choices=('prepend', 'inside', 'none'),
+                        help="Mode of composers generation, default=prepend")
+    parser.add_argument("-o", "--output", help="Name of the output file. If not given, use stdout")
     parser.add_argument("input", nargs="+", help="Directory or CUE file to process")
     args = parser.parse_args()
-    print(args)
+
+    paths_cues = []
+    for in_name in args.input:
+        in_path = pathlib.Path(in_name)
+        if in_path.is_file():
+            paths_cues.append(load_cue(in_path))
+
+    for l in generate_output(args.mode, paths_cues, composers_mode=args.composers):
+        print(l)
     return 0
+
+
+if __name__ == "__main__":
+    main()
